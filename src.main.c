11b3bd83e3a92d9533237c4edac2f23fef5e70e2
dnordyke2-cmyk <dnordyke2@gmail.com>
2025-08-24 11:41:00 -0400

    Create src.main.c

---
+++ src.main.c
@@ -0,0 +1,16 @@
+#include <libdragon.h>
+
+int main(void) {
+    console_init();
+    console_set_render_mode(RENDER_MANUAL);
+    console_clear();
+
+    printf("Shattered Realms (alpha)\n");
+    printf("Build OK. Rendering every 250ms…\n");
+
+    while (1) {
+        console_render();
+        wait_ms(250);
+    }
+    return 0;
+}
